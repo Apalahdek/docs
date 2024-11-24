dfb6fe2a7a1c7c766993d7aebab40deaa63b10bb
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-11-24 19:40:29 +0700

    Update indexData.ts

--- src/data/indexData.ts
+++ src/data/indexData.ts
@@ -1,3 +1,8 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
+document.head.appendChild(link);
+
 export const ProductLinkData = {
   porm: {
     title: "Explore JKT48 Connect API",
